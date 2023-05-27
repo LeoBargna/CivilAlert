c3ccd7146fbc5914a3f0c91d3668fbf0fa4ca6a5
AleVolo12 <volo.ale04@gmail.com>
2023-05-27 09:41:47 +0200

    md5

---
+++ src/routes/attivazione.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useState } from "react";
+import axios from 'axios'
+
+import NavBar from "./navbar"
+import Titolo from "./titolo";
+
+import "../css/attivazione.css"
+
+import img_home from "../assets/home.png"
+import img_attivazione from "../assets/attivazione-colorato.png"
+import img_group from "../assets/gruppo.png"
+import img_logo from "../assets/logo.png"
+import mappa from "../assets/mappa.png"
+
+function Attivazione(){
+
+  const [indirizzo, setIndirizzo] = useState("")
+  const [informazioni, setInformazioni] = useState("")
+  const [tipologia, setTipologia] = useState({})
+  const [emergenza, setEmergenza] = useState({})
+
+  const handelChangeIndirizzo = (event) => {
+    setIndirizzo(event.target.value);
+  }
+
+  const handleChangeInformazioni = (event) => {
+    setInformazioni(event.target.value);
+  }
+
+  const handleChangeTipologia = (event) => {
+    setTipologia(event.target.value);
+  }
+
+  const handleChangeEmergenza = (event) => {
+    setEmergenza(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+
+    // Database
+    /*try{
+      const res = await axios.post("http://localhost:8800/evento/"+indirizzo+"/"+tipologia+"/"+emergenza+"/"+informazioni)
+      console.log(res.data[0])
+      if(res.data.length > 0)
+        navigate("/home?id="+res.data[0].IdVolontarioPk); 
+      else  
+      {
+        console.log(error)
+      }
+    }
+    catch(err){
+      console.log(err)
+    }*/
+
+
+
+    // Telegram
+
+    event.preventDefault();
+    const testo = "❗️ ATTENZIONE, SEGNALAZIONE IN ARRIVO   %0A📍 Punto di ritrovo: " + indirizzo + "%0A 🌋 Tipologia evento: " + tipologia + "%0A🚨 Gravità: P" + emergenza + "%0Aℹ️ Informazioni aggiuntive: " + informazioni + "%0A👉🏻 [Premi per rispondere](https://localhost:5174/?ind="+indirizzo+"&tip="+tipologia+"&eme="+emergenza+")";
+  
+    const token = "6169294690:AAEZ572QSuEal9kUq0dmXtz9wRlRqycbFv8";
+    const canale = "@SegnalazioniPC";
+    const parse = "Markdown";
+    var url = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + canale + "&text=" + testo + "&parse_mode="+ parse;
+
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+    console.log("messaggio inviato");
+    console.log(indirizzo, informazioni, emergenza, tipologia)
+
+  }
+
+  return(
+
+    <div className="attivazione">
+
+      <div className="titolo">
+        <Titolo nomePagina={"Segnalazione"} />
+      </div>
+
+      <div>
+        <img src={mappa} className="mappa"></img>
+      </div>
+
+      <div className="info">
+        <form onSubmit={handleSubmit}>
+          <input 
+            className="input" type="text" name='indirizzo' 
+            value={indirizzo || ""} onChange={handelChangeIndirizzo} 
+            placeholder='Inserisci indirizzo...'/>
+          <input className="input" type="text" name='info' 
+            value={informazioni || ""} onChange={handleChangeInformazioni} 
+            placeholder='Informazioni aggiuntive...'/>
+          <legend className="legenda">Gravità:</legend>
+          <label className="container">P1 - basso
+            <input type="checkbox" value="1" onChange={handleChangeEmergenza}/>
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">P2 - medio basso
+            <input type="checkbox" value="2" onChange={handleChangeEmergenza}/>
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">P3 - medio alto
+            <input type="checkbox" value="3" onChange={handleChangeEmergenza}/>
+            <span className="checkmark"></span>
+          </label>
+          <label className="container">P4 - alto
+            <input type="checkbox" value="4" onChange={handleChangeEmergenza}/>
+            <span className="checkmark"></span>
+          </label>
+          <div className="container-legend">
+            <legend align="left" className="legenda">Tipologia:</legend>
+          </div>
+          <div className="container">
+            <div className="container-select">
+              <select value={tipologia} onChange={handleChangeTipologia} name="tipologia">
+                <option></option>
+                <option value="sisma">Sisma</option>
+                <option value="alluvione">Alluvione</option>
+                <option value="maremoto">Maremoto</option>
+                <option value="incendio">Incendio</option>
+                <option value="sanitaria">Emergenza Sanitaria</option>
+                <option value="chimico">Rischio Chimico</option>
+              </select>
+            </div>
+          </div>
+          <input type="submit" className="bottone" value="INVIA SEGNALAZIONE" />
+        </form>
+      </div>
+      <div className="bottom-navbar">
+        <NavBar home={img_home} attivazione={img_attivazione} account={img_group}/>
+      </div>
+    </div>
+  )
+}
+
+export default Attivazione
+
---
+++ src/routes/login.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import logoPC from '../assets/logo.png'
+import '../css/login.css'
+import TextField from '@mui/material/TextField'
+import { useNavigate } from "react-router-dom";
+import MD5 from 'crypto-js/md5'
+import axios from 'axios'
+
+
+function Login() {
+
+  const [error, setError] = useState(0);
+  
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [utente, setUtente] = useState([])
+
+  const styles = theme => ({
+    textField: {
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto',            
+        paddingBottom: 0,
+        marginTop: 0,
+        fontWeight: 500
+    },
+    input: {
+        color: 'white'
+    }
+});
+
+  const handleClick = async e => {
+    e.preventDefault()
+      /*await axios.get("https://protezione-civile-server.onrender.com/accessoTenda")
+        .then(
+            (res)=>{res.data[user]==password?navigate("./segnalazione"):setError(1)}
+        );*/
+      await axios.get("https://protezione-civile-server.onrender.com/accessoTenda")
+        .then(
+            (res)=>{
+                // res.data[user]==MD5(pw)?navigate("../show?user="+user):setError(1)
+                if(
+                    MD5(password).words[0] == res.data[user].words[0]&&
+                    MD5(password).words[1] == res.data[user].words[1]&&
+                    MD5(password).words[2] == res.data[user].words[2]&&
+                    MD5(password).words[3] == res.data[user].words[3]
+                )
+                {
+                    navigate("./attivazione")
+                }
+                else
+                {
+                  setError(1)
+                }
+            }
+        );
+  }
+
+  
+
+  const handleChangeUser = (event) => {
+    setUser(event.target.value);
+  }
+
+  const handleChangePw = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const navigate = useNavigate()
+
+  return (
+
+    <div style={{background: "white", height: "100vh", width: "100vw"}} className="Login">
+
+      <div className="contImmagine">
+        <img className="logoPC" src={logoPC}/>
+      </div>
+
+      <fieldset className = "form">
+        <legend>Login</legend>
+        <input className="input" type="text" name='user' placeholder='username' onChange={handleChangeUser}/>
+        <input className="input" type="password" name='password' placeholder='password' onChange={handleChangePw}/>
+      </fieldset>
+
+      <div>
+        <form>
+          <button type="submit" onClick={handleClick} className='bottone-login'>
+            ACCEDI
+          </button>  
+        </form> 
+      </div>
+
+      {error?<div className="error">Nome Utente o Password errati</div>:null}
+
+    </div>
+
+  )
+}
+
+export default Login
